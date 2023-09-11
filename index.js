d8174d2efceb7e02a20c791d78fbfd0c42e21f4e
Matt Tucker <arcanemahuloq@gmail.com>
2023-09-10 19:17:48 -0500

    Grasped the nuances of 'this' in JavaScript

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ class User {
 
 const hanSolo = new User("Han Solo", 100, 50, ["Blaster"]);
 console.log(hanSolo);
+
+const userTest = {
+  firstName: "Matt",
+  lastName: "Tucker",
+  fullname: function () {
+    console.log(this);
+    console.log(this.firstName + " " + this.lastName);
+  },
+};
+userTest.fullname();
+
+const userTest2 = {
+  firstName: "Matt",
+  lastName: "Tucker",
+  fullname: () => {
+    console.log(this);
+    console.log(this.firstName + " " + this.lastName);
+  },
+};
+//Arrow function grabs nearest function for this, so it grabs the global window
+userTest2.fullname();
